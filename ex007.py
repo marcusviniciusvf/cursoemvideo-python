cf036f9ef3e9a6da01b02700972b5d6e9f4c4996
marcusviniciusvf <marcusviniciusvf@users.noreply.github.com>
2025-05-14 09:32:43 -0300

    ex007 Média Aritmética

---
+++ ex007.py
@@ -0,0 +1,6 @@
+nota_1 = float(input("Digite a primeira nota do aluno: "))
+nota_2 = float(input("Digite a segunda nota do aluno: "))
+
+nota_media= (nota_1+nota_2)/2
+
+print("A média é: {}".format(nota_media))
