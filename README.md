d8fc76ba38d77bb7e834299dd7f129f385b82cde
marcusviniciusvf <marcusviniciusvf@users.noreply.github.com>
2025-05-17 11:08:28 -0300

    ex008 Conversor de Medidas

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cursoemvideo-python
 Versionamento dos estudos com auxilio do canal Curso em Vídeo, na linguagem Python.
+Repositório criado para estudos na linguagem python, seguindo os ensinamentos do canal "Curso em vídeo"
---
+++ ex008.py
@@ -0,0 +1,10 @@
+dist_Meters = int(input("Digite a distância em metros: "))
+
+print(dist_Meters,"metros equivalem a :\n",
+      dist_Meters/1000,'km\n',
+      dist_Meters/100,'hm\n',
+      dist_Meters/10,'dam\n',
+      dist_Meters*10,'dm\n',
+      dist_Meters*100,'cm\n',
+      dist_Meters*1000,'mm\n'
+      )
