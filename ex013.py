5a0c7f9b5bed86a4b81656dcd4d37da0110830de
marcusviniciusvf <marcusviniciusvf@users.noreply.github.com>
2025-05-18 20:40:11 -0300

    ex013 Reajuste Salarial

---
+++ ex013.py
@@ -0,0 +1,5 @@
+#Reajuste Salarial
+sal_Value = float(input("Digite o salário do funcionário a ser reajustado: "))
+sal_Up = sal_Value*1.15
+
+print(f"O salário do funcionario com reajuste de 15% é de R${sal_Up}.")
