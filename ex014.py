7912f0c9c49465f5e30bb282ac623b99254ac09c
marcusviniciusvf <marcusviniciusvf@users.noreply.github.com>
2025-05-19 09:22:15 -0300

    ex014 Conversor de Temperaturas

---
+++ ex014.py
@@ -0,0 +1,6 @@
+#CONVERSOR DE TEMPERATURAS
+temp_Celsius = float(input("Informe a temperatura em °C: "))
+
+temp_Fahren = (temp_Celsius*(9/5))+32
+
+print(f"A temperatura de {temp_Celsius}°C em Fahrenheit é de {temp_Fahren}F")
