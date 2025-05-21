b807d3be9724ca35e74123d7d8814c2d435cd5ff
marcusviniciusvf <marcusviniciusvf@users.noreply.github.com>
2025-05-21 09:31:42 -0300

    ex030 Par ou Ímpar?

---
+++ ex030.py
@@ -0,0 +1,5 @@
+#Exercício Python 030: Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+n = int(input("Digite seu numero: "))
+print('Par' if n%2==0
+      else 'Ímpar')
