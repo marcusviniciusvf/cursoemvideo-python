66c4727971a53dfb4e703cb0723364532d5ba3eb
marcusviniciusvf <marcusviniciusvf@users.noreply.github.com>
2025-05-23 08:43:15 -0300

    edit e037 Conversor de Bases Numéricas

--- ex037.py
+++ ex037.py
@@ -1,9 +1,10 @@
-#Exercício Python 037: Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher
-#  qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
-#Conversor de Bases Numéricas
-
-numero = int(input("Digite um número inteiro: "))
-menu_Conversao = 0
-menu_Conversao = int(input("[1] para conversão em BINARIO\n[2] para conversão em OCTAL\n[3] para conversão em HEXADECIMAL" while menu_Conversao not in [1,2,3] else ""))
-
-print(menu_Conversao)
+#Exercício Python 037: Escreva um programa em Python que leia um número inteiro qualquer
+#e peça para o usuário escolher qual será a base de conversão:
+#1 para binário, 2 para octal e 3 para hexadecimal.
+#037 Conversor de Bases Numéricas
+num = int(input("Digite seu numero: "))
+opcao = int(input("""[1] para BINARIO
+[2] para OCTAL
+[3] para HEXADECIMAL
+Digite sua opção: """))
+print(bin(num)[2:] if opcao==1 else oct(num)[2:] if opcao==2 else hex(num)[2:] if opcao==3 else 'Opcao invalida')
